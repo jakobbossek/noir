2a700a081b60017f8a2fb3b2f07353998876d706
Jakob Bossek <info@jakobbossek.de>
2016-08-16 17:57:28 +0200

    add docs to internallyRegisterOptimizer

--- R/internallyRegisterOptimizer.R
+++ R/internallyRegisterOptimizer.R
@@ -1,3 +1,31 @@
+#' @title
+#' Register a new optimizer / optimization algorithm.
+#'
+#' @description
+#' This function is the \dQuote{make} of \pkg{noir}. It is a constructor for
+#' \pkg{noir}-optimizers.
+#'
+#' @param id [\code{character(1)}]\cr
+#'   String representation of the optimizer.
+#FIXME: later add note, that this is used in runOptimizer() or however I am going
+# to call the method ;-)
+#' @param class [\code{character(1)}]\cr
+#'   Class used internally for the optimizer. Basically this can be equal to \code{id}.
+#' @param packages [\code{character}*]\cr
+#'   Packages needed to execute the optimizer.
+#'   Default is \code{NULL}, i.e., base R is sufficient.
+#' @param hyper.par.set [\code{\link[ParamHelpers]{Paramset}}]\cr
+#'   Parameter set defining the algorithm parameters.
+#'   See \code{\link[ParamHelpers]{makeParamSet}} for details.
+#' @param objective.type [\code{character(1)}]\cr
+#'   One of \dQuote{single-objective} or \dQuote{multi-objective}.
+#'   See \code{\link{getAvailableObjectiveTypes}}.
+#' @param tags [\code{character(1)}]\cr
+#'   Keywords which characterize the properties of the optimizer.
+#'   See \code{\link{getSupportedTags}} for a list of available tags.
+#'   Default is \code{NULL}, i.e., no tags are assigned.
+#' @return [\code{optimizer}] S3 object encapsulating the optimizer.
+#' @export
 internallyRegisterOptimizer = function(
 	id,
 	class,
